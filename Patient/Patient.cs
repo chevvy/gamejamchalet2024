18cf88420537ad8aedb39427407f61d40c4df1b8
The Greek <greeklovesmaple1414@gmail.com>
2024-08-03 17:08:14 -0400

    Patients have demands!

--- Patient/Patient.cs
+++ Patient/Patient.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Patient : RigidBody2D
 {
@@ -7,14 +9,32 @@ public partial class Patient : RigidBody2D
     [Signal]
     public delegate void OnPatientDeadEventHandler();
 
+    [Signal]
+    public delegate void OnPatientSavedEventHandler();
+
     private const int HEALTH_AMT = 1;
+
+    [Export]
+    private const int HOW_MANY_DEMANDS = 3;
 
     private Node2D bars;
     private LifeBar lifeBar;
 
     private Timer healthTimer;
+    private Timer demandsTimer;
+    private Timer timeUntilDeadTimer;
+
+    private Node2D itemHolder;
+    private Sprite2D itemDemanded;
+
+    private List<ClosetItemType> demands = new();
+    private ClosetItemType? currentDemand = null;
 
     private bool isAlive = true;
+
+    private AnimationPlayer player;
+
+
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -23,17 +43,25 @@ public partial class Patient : RigidBody2D
         lifeBar = GetNode<LifeBar>("Bars/LifeBar");
         healthTimer = GetNode<Timer>("HealingFromItemTimer");
 
+        demandsTimer = GetNode<Timer>("DemandsTimer");
+        timeUntilDeadTimer = GetNode<Timer>("TimeUntilDeadTimer");
+        itemHolder = GetNode<Node2D>("ItemHolder");
+        itemDemanded = GetNode<Sprite2D>("ItemHolder/ItemDemanded");
+        player = GetNode<AnimationPlayer>("AnimationPlayer");
+
         if (GameManager.Instance != null)
         {
             GameManager.Instance.GameReady += Initialize;
         }
 
-        lifeBar.OnLifeZero += Die;
+        //lifeBar.OnLifeZero += Die;
     }
 
     private void Initialize()
     {
+        CreateRandomDemands();
         healthTimer.Start();
+        demandsTimer.Start();
     }
 
     private void OnTreeExiting()
@@ -46,8 +74,13 @@ public partial class Patient : RigidBody2D
 
     public void ReceiveItem(ClosetItemType item)
     {
-        const int ITEM_VALUE = 25;
+        if (currentDemand.HasValue && item == currentDemand)
+        {
+            DemandMet();
+        }
 
+        // Old mechanic ...
+        const int ITEM_VALUE = 25;
         foreach (Node child in bars.GetChildren())
         {
             if (child is DecrementingBar bar && bar.itemType == item)
@@ -68,6 +101,70 @@ public partial class Patient : RigidBody2D
         }
     }
 
+    private void CreateRandomDemands()
+    {
+        Random r = new Random();
+        for (int i = 0; i < HOW_MANY_DEMANDS; ++i)
+        {
+            demands.Add((ClosetItemType)r.Next(3));
+        }
+    }
+
+    private void OnDemandTimerTimeout()
+    {
+        if (demands.Count == 0)
+        {
+            GD.Print("Patient saved!");
+            return;
+        }
+        currentDemand = demands.First();
+        demands.RemoveAt(0);
+        demandsTimer.Stop();
+
+        StartDemand();
+    }
+
+    private void StartDemand()
+    {
+        if (!currentDemand.HasValue)
+        {
+            GD.PrintErr("Incorrect patient demand ...");
+            return;
+        }
+
+        itemHolder.Visible = true;
+        string itemName = "";
+        if (currentDemand == ClosetItemType.PILLZ)
+        {
+            itemName = "Pills";
+        }
+        if (currentDemand == ClosetItemType.SERINGE)
+        {
+            itemName = "Syringe";
+        }
+        if (currentDemand == ClosetItemType.BANDAGE)
+        {
+            itemName = "BandAid";
+        }
+        itemDemanded.Texture = (Texture2D)GD.Load($"res://Assets/items/Asset_PickUp_{itemName}.png");
+
+        timeUntilDeadTimer.Start();
+        player.Play("hurry");
+    }
+
+    private void DemandMet()
+    {
+        currentDemand = null;
+        itemHolder.Visible = false;
+        player.Stop();
+        demandsTimer.Start();
+    }
+
+    private void OnDeathTimer()
+    {
+        Die();
+    }
+
     private void OnHealthTickTimeout()
     {
         foreach (Node child in bars.GetChildren())
@@ -86,6 +183,16 @@ public partial class Patient : RigidBody2D
     private void GainHealthFromTick()
     {
         lifeBar.Increase(HEALTH_AMT);
+    }
+
+    private void PatientSaved()
+    {
+        if (isAlive)
+        {
+            EmitSignal(SignalName.OnPatientSaved);
+            GetParent().RemoveChild(this);
+            QueueFree(); //TODO Dead animation or remove the sprite ???
+        }
     }
 
     private void Die()
