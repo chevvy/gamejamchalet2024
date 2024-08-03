4582e684e128affad6a50d7486727097dfeed689
The Greek <greeklovesmaple1414@gmail.com>
2024-08-02 21:55:51 -0400

    Colliding with patient can apply item

--- Patient/Patient.cs
+++ Patient/Patient.cs
@@ -4,31 +4,32 @@ using System;
 public partial class Patient : RigidBody2D
 {
 
-	private DecrementingBar decrementingBar;
-	private LifeBar lifeBar;
+    private DecrementingBar decrementingBar;
+    private LifeBar lifeBar;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		decrementingBar = GetNode<DecrementingBar>("Bars/DecrementingBar");
-		lifeBar = GetNode<LifeBar>("Bars/LifeBar");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        decrementingBar = GetNode<DecrementingBar>("Bars/DecrementingBar");
+        lifeBar = GetNode<LifeBar>("Bars/LifeBar");
+    }
 
-	public void GainHealthFromItem(int value)
-	{
-		lifeBar.Increase(value);
-	}
+    public void ReceiveItem()
+    {
+        const int ITEM_VALUE = 25;
+        decrementingBar.Increase(ITEM_VALUE);
+    }
 
-	private void OnHealthTickTimeout()
-	{
-		if (!decrementingBar.IsEmpty())
-		{
-			GainHealthFromTick();
-		}
-	}
+    private void OnHealthTickTimeout()
+    {
+        if (!decrementingBar.IsEmpty())
+        {
+            GainHealthFromTick();
+        }
+    }
 
-	private void GainHealthFromTick()
-	{
-		lifeBar.Increase(5);
-	}
+    private void GainHealthFromTick()
+    {
+        lifeBar.Increase(5);
+    }
 }
