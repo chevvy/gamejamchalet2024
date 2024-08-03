65f089b87801af421acd8ab8992d01bd1dbc7058
Alix Lelièvre <lelievre.alix@gmail.com>
2024-08-03 15:22:27 -0400

    WIP (#20)

---
+++ Closet/Closet.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Closet : RigidBody2D
+{
+	[Export] public ClosetItemType ClosetItemType;
+
+	private ClosetPickupAreaCollisionHandler _closetPickupAreaCollisionHandler;
+
+	public override void _Ready()
+	{
+		GD.Print("_Ready on ClosetGameReadyStateHandler");
+
+		GameManager.Instance.GameReady += OnGameReady;
+
+		SetClosetItemType(ClosetItemType);
+
+		Freeze = true;
+	}
+
+	private void SetClosetItemType(ClosetItemType itemType)
+	{
+		_closetPickupAreaCollisionHandler = GetNode<CollisionObject2D>("ClosetPickupArea") as ClosetPickupAreaCollisionHandler;
+		_closetPickupAreaCollisionHandler.ClosetItemType = itemType;
+	}
+
+	public void OnGameReady()
+	{
+		GD.Print("Game ready on ClosetGameReadyStateHandler");
+
+		FreezeMode = FreezeModeEnum.Static;
+		Freeze = false;
+	}
+
+	private void OnTreeExiting()
+	{
+		GameManager.Instance.GameReady -= OnGameReady;
+	}
+}
--- Closet/ClosetPickupAreaCollisionHandler.cs
+++ Closet/ClosetPickupAreaCollisionHandler.cs
@@ -2,7 +2,7 @@ using Godot;
 
 public partial class ClosetPickupAreaCollisionHandler : Area2D
 {
-	[Export] public ClosetItemType ClosetItemType;
+	public ClosetItemType ClosetItemType;
 
 	public void OnBodyEntered(Node2D body)
 	{
