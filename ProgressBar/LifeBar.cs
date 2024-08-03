bbe28434484d509fc2454f3b4972c32577c93eb8
The Greek <greeklovesmaple1414@gmail.com>
2024-08-03 11:48:10 -0400

    Patient loses life from separate life timer

--- ProgressBar/LifeBar.cs
+++ ProgressBar/LifeBar.cs
@@ -3,18 +3,38 @@ using System;
 
 public partial class LifeBar : Control
 {
+	[Export]
+	public const int LOSE_LIFE_PER_TICK = 1;
+
 	private ProgressBar bar;
+
+	private Timer lifeTimer;
+
 	public override void _Ready()
 	{
 		bar = GetNode<ProgressBar>("ProgressBar");
+		lifeTimer = GetNode<Timer>("PassifLifeLossTimer");
+
+		GameManager.Instance.GameReady += Initialize;
 	}
 
-	public override void _Process(double delta)
+	private void Initialize()
 	{
+		lifeTimer.Start();
+	}
+
+	private void OnTreeExiting()
+	{
+		GameManager.Instance.GameReady -= Initialize;
 	}
 
 	public void Increase(int value)
 	{
 		bar.Value += value;
+	}
+
+	public void LoseLifeFromTimer()
+	{
+		bar.Value -= LOSE_LIFE_PER_TICK;
 	}
 }
