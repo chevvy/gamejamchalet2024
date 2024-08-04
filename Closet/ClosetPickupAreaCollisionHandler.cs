c741fc818d28133298f95e443b835fda538d94a3
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2024-08-03 20:37:57 -0400

    Input for closet (#45)
    * action to use item
    
    * now can switch item
    
    * clean up

--- Closet/ClosetPickupAreaCollisionHandler.cs
+++ Closet/ClosetPickupAreaCollisionHandler.cs
@@ -6,14 +6,18 @@ public partial class ClosetPickupAreaCollisionHandler : Area2D
 
 	public void OnBodyEntered(Node2D body)
 	{
-		GD.Print(string.Format("{0}.OnBodyEntered: {1} - ClosetItemType: {2}", this.Name, body.Name, ClosetItemType.ToString()));
-
 		if (body is Character character) OnEnterClosetPickupArea(character);
 	}
 
 	private void OnEnterClosetPickupArea(Character character)
 	{
-		character.ReceiveItem(ClosetItemType);
+		character.CanReceiveItem = true;
+		character.ItemType = ClosetItemType;
+	}
+
+	private void OnExitClosetPickupArea(Character character)
+	{
+		character.ItemType = null;
 	}
 
 	private void OnDestroy()
