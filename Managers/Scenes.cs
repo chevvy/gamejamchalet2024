23d83434d8b9c374e82997c6fd0a4ad78524cdf7
Alix Lelièvre <lelievre.alix@gmail.com>
2024-08-03 22:22:16 -0400

    Make scenes path CONSTANTS

---
+++ Managers/Scenes.cs
@@ -0,0 +1,6 @@
+public static class Scenes
+{
+    public static readonly string MENU = "res://Levels/Menu/Menu.tscn";
+    public static readonly string CREDIT = "res://Levels/Menu/Credit.tscn";
+    public static readonly string GAME = "res://main.tscn";
+}
