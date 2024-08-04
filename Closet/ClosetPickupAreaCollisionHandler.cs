77ee5d418d109c118fd6b8814f5802301383f370
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2024-08-03 20:37:39 -0400

    clean up

--- Closet/ClosetPickupAreaCollisionHandler.cs
+++ Closet/ClosetPickupAreaCollisionHandler.cs
@@ -6,28 +6,17 @@ public partial class ClosetPickupAreaCollisionHandler : Area2D
 
 	public void OnBodyEntered(Node2D body)
 	{
-		// GD.Print(string.Format("{0}.OnBodyEntered: {1} - ClosetItemType: {2}", this.Name, body.Name, ClosetItemType.ToString()));
-
 		if (body is Character character) OnEnterClosetPickupArea(character);
-	}
-
-	public void OnBodyExited(Node2D body)
-	{
-		
-		// if (body is Character character) OnExitClosetPickupArea(character);
-		
 	}
 
 	private void OnEnterClosetPickupArea(Character character)
 	{
 		character.CanReceiveItem = true;
 		character.ItemType = ClosetItemType;
-		// character.ReceiveItem(ClosetItemType);
 	}
 
 	private void OnExitClosetPickupArea(Character character)
 	{
-		
 		character.ItemType = null;
 	}
 
