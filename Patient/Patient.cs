081289f07e5fedd299b29dd23079daef2a2164d8
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2024-08-05 19:44:28 -0400

    clean up audio loop

--- Patient/Patient.cs
+++ Patient/Patient.cs
@@ -184,9 +184,10 @@ public partial class Patient : RigidBody2D
 
             patientSprite.Texture = BED_EMPTY_TEXT;
             itemHolder.Visible = false;
-
-            //GetParent().RemoveChild(this);
-            //QueueFree(); //TODO Dead animation or remove the sprite ???
+            demandsTimer.Stop();
+            timeUntilDeadTimer.Stop();
+            timeUntilShakeTimer.Stop();
+            audioDeathEffectPlayer.Stop();
         }
     }
 }
